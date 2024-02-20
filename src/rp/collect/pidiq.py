5617e0442caf85ead244d09eb16aeee1a8c13d89
Edoardo Fazzari <46302551+edofazza@users.noreply.github.com>
2024-02-20 15:39:21 -0600

    collect dataset

---
+++ src/rp/collect/pidiq.py
@@ -0,0 +1,39 @@
+import time
+import numpy as np
+from src.rp.rppid import RedPitayaPID
+
+
+class PIDIQ(RedPitayaPID):
+    def __init__(self, hostname: str):
+        super().__init__(hostname)
+        self.init_time = time.time()
+        self.counter = 1
+
+    def analyze(self):
+        if self.init_time + 604800 > self.init_time:
+            return
+        self.reset()
+        self.ramp_piezo()
+        if self.scan_temperature(500):
+            time.sleep(10)
+            self.lock_cavity()
+            starting_time = time.time()
+            print(f'Locked at: {starting_time}')
+            while True:
+                time.sleep(10)
+                print(f'Seconds after start: {time.time() - starting_time}')
+                out1, iq0 = self.scope(input2='iq0')
+                _, in2 = self.scope()
+
+                if in2.max() < 0.95:
+                    end_time = time.time()
+                    print(f'Lost lock at: {end_time}. Took {end_time-starting_time} ({self.counter})')
+                    break
+                else:
+                    np.save(f'dataset/iq0/{self.counter}.npy', iq0)
+                    if self.counter < 100:
+                        np.save(f'dataset/out1/{self.counter}.npy', out1)
+                    self.counter += 1
+                    print(f'purple (fast) signal mean: {out1.mean()}')
+                    print(f'blue signal mean: {in2.mean()}')
+        self.analyze()
