819599226b4b4f10f1f0529119eb97ed541d6929
Edoardo Fazzari <46302551+edofazza@users.noreply.github.com>
2024-05-06 10:32:47 -0500

    test

--- src/rp/run.py
+++ src/rp/run.py
@@ -5,7 +5,7 @@ from stable_baselines3.common.callbacks import CheckpointCallback
 
 from redpitayaenv import RedPitayaEnv
 from rpscope import RedPitayaScope
-from pyrpl import RedPitaya
+from pyrpl import Pyrpl
 
 def create_env(skip: int = 15):
     #env = RedPitayaEnv(RedPitayaScope('169.254.167.128'))
@@ -55,5 +55,5 @@ def run():
 
 
 if __name__ == '__main__':
-    p = RedPitaya(hostname='169.254.167.128')
+    p = Pyrpl(hostname='169.254.167.128')
     #run()
